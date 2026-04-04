5181010c2ab3ea67edee127e7ab777466393457a
RANGAM JAGADESH <jashu9392@gmail.com>
2026-04-04 11:41:32 +0530

    created Trend model

--- trends/models.py
+++ trends/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Trend(models.Model):
+    title = models.CharField(max_length=300)
+    source = models.CharField(max_length=100)
+    url = models.URLField()
+    summary = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
